6c6a8acffc488455d02b5496abac3acaeb0df963
ismael-haque <ismaelhaque.dev@gmail.com>
2023-07-25 19:36:07 +0600

    end of pass by ref

--- passByRef.py
+++ passByRef.py
@@ -22,5 +22,6 @@ print(ford.name)
 # Main takeaway from this set of code is the changes we make to a object through other functions or any other ways also changes the object's property/attribute and the object's memory location or reference id does not change
 
 # this is called mutatble object as after changing an object it's reference id or memory location still hasn't changed
+# So object of a class is mutable
 # list and dictionary is mutable
 # tuple is immutable
