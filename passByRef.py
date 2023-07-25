376a97140fe62d6e39835db4f2e5d8166c321643
ismael-haque <ismaelhaque.dev@gmail.com>
2023-07-25 19:06:59 +0600

    edited pass by ref

--- passByRef.py
+++ passByRef.py
@@ -11,6 +11,7 @@ def feature(car, new_name):
     print("Name : ", car.name)
     print(id(car))
     car.name = new_name
+    print(id(car))
 
 feature(ford, "Audi")
 
@@ -18,8 +19,8 @@ print(id(ford))
 
 print(ford.name)
 
-# Main takeaway from this set of code is the changes we make to a object through other functions or any other ways also changes the object's property/attribute as they have same reference id (or place in memory)
+# Main takeaway from this set of code is the changes we make to a object through other functions or any other ways also changes the object's property/attribute and the object's memory location or reference id does not change
 
-# this is called mutatble object
+# this is called mutatble object as after changing an object it's reference id or memory location still hasn't changed
 # list and dictionary is mutable
 # tuple is immutable
